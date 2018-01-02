14be519b0c66a661b5c1d691aa82c8e40f00f29e
holly <hbrobst@gmail.com>
2018-01-02 07:23:31 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,19 +4,27 @@ console.log("up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
+var checkForMatch = function () {
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("you found a match!");
 	} else  {
 		alert('sorry try again');
 	}
+ };
+
+checkForMatch();
+
+
+var flipCard = function(cardId) {
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2) {
+ checkForMatch();
+}
 };
+
+flipCard(0);
+flipCard(2);
